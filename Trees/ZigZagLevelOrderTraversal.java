9a9b3c2336ef32b5f9dd5cc24b2a21d5fd218640
Sai Phani Krishna Pasumarthi <27241805+saiphani1998@users.noreply.github.com>
2020-07-21 23:49:51 +0530

    Added solution to return the zigzag level order traversal of its nodes’ values.

---
+++ Trees/ZigZagLevelOrderTraversal.java
@@ -0,0 +1,79 @@
+/*
+* Given a binary tree, return the zigzag level order traversal of its nodes’ values.
+* (ie, from left to right, then right to left for the next level and alternate between).
+
+* Example :
+    Given binary tree
+        3
+       / \
+      9  20
+        /  \
+       15   7
+    return
+    [
+         [3],
+         [20, 9],
+         [15, 7]
+    ]
+* */
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    Map<Integer,ArrayList<Integer>> map = new HashMap<>();
+    ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode A) {
+        int h = height(A);
+
+        for (int i=1;i<=h; i++) {
+            populateMap(A, i, 1);
+        }
+
+        for (int i=1;i<=h;i++) {
+            ArrayList<Integer> temp = map.get(i);
+            if (i%2 == 0) {
+                Collections.reverse(temp);
+            }
+            ans.add(temp);
+        }
+
+        return ans;
+    }
+
+    public void populateMap(TreeNode root, int level, int actualLevel) {
+        if (root == null) return;
+        if (level == 1) {
+            ArrayList<Integer> temp;
+            if (!map.containsKey(actualLevel)) {
+                temp = new ArrayList<>();
+            }
+            else {
+                temp = map.get(actualLevel);
+            }
+
+            temp.add(root.val);
+            map.put(actualLevel, temp);
+        }
+        else {
+            populateMap(root.left, level-1, actualLevel+1);
+            populateMap(root.right, level-1, actualLevel+1);
+        }
+    }
+
+    public int height(TreeNode A) {
+        if (A == null) return 0;
+        return 1 + Math.max(height(A.left), height(A.right));
+    }
+}
